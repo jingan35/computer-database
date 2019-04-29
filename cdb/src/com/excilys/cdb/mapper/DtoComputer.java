d60fd83e506beb758542fafedd4df918c89f60d8
jingan35 <jude_ingani@hotmail.com>
2019-04-29 10:57:34 +0200

    equals et hashcode implemantés pour les model et dto

--- cdb/src/com/excilys/cdb/mapper/DtoComputer.java
+++ cdb/src/com/excilys/cdb/mapper/DtoComputer.java
@@ -91,5 +91,57 @@ public class DtoComputer {
 		s=s+"id="+id+" name: "+name+ " introduced Time:"+introduced+ " discontinued Time:"+ discontinued+ " company_name= "+companyName;
 		return s;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company_id == null) ? 0 : company_id.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DtoComputer other = (DtoComputer) obj;
+		if (company_id == null) {
+			if (other.company_id != null)
+				return false;
+		} else if (!company_id.equals(other.company_id))
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
 
 }
--- cdb/src/com/excilys/cdb/model/ModelComputer.java
+++ cdb/src/com/excilys/cdb/model/ModelComputer.java
@@ -86,4 +86,53 @@ public class ModelComputer {
 		// TODO Auto-generated method stub
 		company_id=newCompanyId;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company_id == null) ? 0 : company_id.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModelComputer other = (ModelComputer) obj;
+		if (company_id == null) {
+			if (other.company_id != null)
+				return false;
+		} else if (!company_id.equals(other.company_id))
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id != other.id)
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
